9c0bb1e885cb44bab2d3761a5ea26fbd1e21e3cd
KPS_DESKTOP <dev.kps88@gmail.com>
2025-01-01 20:37:15 +0900

    chapter1 commit

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,26 +1,12 @@
 <template>
   <div>
-    <button v-on:click="increments">Counter: {{ counter }}</button>
+     
   </div>
 </template>
 
 <script>
   export default {
-    data() {
-      return  {
-        counter: 0
-      }
-    }
-    ,
-    methods: {
-      increments() {
-        this.counter++
-      }
-    },
-    mounted() {
-      console.log("#### 마운트 완료");
-    }
-      
+    
   }
 </script>
 
