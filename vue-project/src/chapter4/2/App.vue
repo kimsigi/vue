f7706e975f29211c97fa00d63d7f0641aba6208d
KPS_DESKTOP <dev.kps88@gmail.com>
2025-01-12 22:42:12 +0900

    4-2강 완료

---
+++ vue-project/src/chapter4/2/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <p>카운트: {{ count }}</p>
+
+        <button v-on:click="count++">카운트 직접</button>
+        <button v-on:click="increment">카운트 함수</button>
+    </div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue';
+export default {
+    props: {
+        title: String
+    },
+
+    setup (props, context) {   
+        const count = ref(0);
+        const increment = () => count.value++;
+
+        console.log(props.title);
+
+        console.log("### context: ", context);
+        console.log("### context.attrs: ", context.attrs);
+        console.log("### context.slots: ", context.slots);
+        console.log("### context.emit: ", context.emit);
+        console.log("### context.expose: ", context.expose);
+
+        return {
+            count,
+            increment,
+            onMounted
+        }
+    },
+
+    // 셋업 바깥에 있다.    
+    mounted() {
+        console.log(this.count);
+    }
+
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 //import App from './App.vue'               // index
-import App from '@/chapter/1/App.vue'
+//import App from '@/chapter4/1/App.vue'
+import App from '@/chapter4/2/App.vue'
 
 createApp(App).mount('#app')
