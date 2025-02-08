0b2bc09cb88304ce0059226f47501190881a23d5
= <=>
2025-02-09 01:47:20 +0900

    4-6 커밋2222

--- vue-project/src/chapter4/6/App.vue
+++ vue-project/src/chapter4/6/App.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-        테스트: {{ test }}
+        <h2>HTML 클래스 바인딩</h2>
+        <p>
+            
+        </p>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+    import { ref } from 'vue';
 
     const test = ref(1);
+
+    
 </script>
 
 <style lang="scss" scoped>
