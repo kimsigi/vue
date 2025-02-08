4a0b8c2b115779909495618db57560cb0246111f
= <=>
2025-02-09 01:57:49 +0900

    노트북 커밋

--- vue-project/src/chapter4/6/App.vue
+++ vue-project/src/chapter4/6/App.vue
@@ -3,6 +3,7 @@
         <h2>HTML 클래스 바인딩</h2>
         <p>
             <strong>객체 바인딩</strong>
+            <p>클래스를 동적으로 바인딩 하기위해선는 :class(v-bind:class)를 사용할 수 있습니다.</p>
         </p>
     </div>
 </template>
@@ -11,7 +12,7 @@
     import { ref } from 'vue';
 
     const test = ref(1);
-
+    console.log("### test", test);
     
 </script>
 
