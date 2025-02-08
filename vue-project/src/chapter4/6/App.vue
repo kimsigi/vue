3c5e8160bbcdb775331c2cb62fa2abd637e6eb2f
KPS_DESKTOP <dev.kps88@gmail.com>
2025-02-09 02:11:20 +0900

    commit

--- vue-project/src/chapter4/6/App.vue
+++ vue-project/src/chapter4/6/App.vue
@@ -2,8 +2,11 @@
     <div>
         <h2>HTML 클래스 바인딩</h2>
         <p>
-            <strong>객체 바인딩</strong>
-            <p>클래스를 동적으로 바인딩 하기위해선는 :class(v-bind:class)를 사용할 수 있습니다.</p>
+            <strong>객체 바인딩</strong> 
+            <p>
+                클래스를 동적으로 바인딩 하기위해선는 :class(v-bind:class)를 사용할 수 있습니다.
+            </p>
+            <div class="text" :class="{ active: isActive, 'text-danger': hasError }"></div>
         </p>
     </div>
 </template>
@@ -11,8 +14,8 @@
 <script setup>
     import { ref } from 'vue';
 
-    const test = ref(1);
-    console.log("### test", test);
+    const isActive = ref(true);
+    const hasError = ref(false);
     
 </script>
 
