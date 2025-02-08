1e24c87fd67451eb2662467071eb04a8efb0219e
= <=>
2025-02-09 01:37:38 +0900

    4-6 디폴트 적용

---
+++ vue-project/src/chapter4/6/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        테스트: {{ test }}
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+    const test = ref(1);
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 //import App from '@/chapter4/2/App.vue'
 //import App from '@/chapter4/3/App.vue'
 //import App from '@/chapter4/4/App.vue'
-import App from '@/chapter4/5/App.vue'
+//import App from '@/chapter4/5/App.vue'
+import App from '@/chapter4/6/App.vue';
 
 createApp(App).mount('#app')
