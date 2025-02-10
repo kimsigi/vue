aeeaef85b427d7d754efc4bcc7e9e9759301b33f
KPS_DESKTOP <dev.kps88@gmail.com>
2025-02-11 00:49:18 +0900

    오늘의 공부 완료

---
+++ vue-project/src/chapter4/8/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in items" :key="item.id">{{ item.message }}</li>
+        </ul>
+        
+        <hr>
+        
+        <ul>
+            <li v-for="(item, index) in items" :key="index">{{index}}==>{{ item.message }}</li>
+        </ul>
+        
+        <hr>
+
+        <ul>
+            <li v-for="(value, key, index) in myObject" :key="index">{{key}}:{{ value }}</li>
+            ==============
+            <li v-for="(value, key, index) in myObject" :key="key">
+                {{ key }} - {{ value }} - {{ index }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+    const items = reactive([
+        { id: 1, message: 'Java' },
+        { id: 2, message: 'HTML' },
+        { id: 3, message: 'CSS' },
+        { id: 4, message: 'JavaScript' },
+    ]);
+
+    const myObject = reactive({
+        title: '제목입니다.',
+        author: '홍길동',
+        publishedAt: '2016-04-10',
+    });
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -5,6 +5,8 @@ import { createApp } from 'vue'
 //import App from '@/chapter4/3/App.vue'
 //import App from '@/chapter4/4/App.vue'
 //import App from '@/chapter4/5/App.vue'
-import App from '@/chapter4/6/App.vue';
+//import App from '@/chapter4/6/App.vue';
+//import App from '@/chapter4/7/App.vue';
+import App from '@/chapter4/8/App.vue';
 
 createApp(App).mount('#app')
