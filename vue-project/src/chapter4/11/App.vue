c72bcc6ffeaf11922638c0e0fd148e681f87cdff
kps <dev.kps88@gmail.com>
2025-02-11 18:04:34 +0900

    Watch, WatchEffect 공부 진행중

--- vue-project/src/chapter4/11/App.vue
+++ vue-project/src/chapter4/11/App.vue
@@ -1,10 +1,45 @@
 <template>
   <div>
     <h1>Watch, WatchEffect</h1>
+    <p>
+      이름: <input class="border-2 border-indigo-600 " type="text" v-model="name" />
+    </p>
+    <p>
+      더하기:
+        <input class="border-2 border-indigo-600 " type="text" v-model.number="a" />
+        <input class="border-2 border-indigo-600 " type="text" v-model.number="b" />
+    </p>
+
   </div>
 </template>
 
 <script setup>
+import { reactive, ref, watch } from 'vue';
+  // 싱글 레프
+  const name = ref('Kim');
+  watch(name, (newValue, oldValue) => console.log("## newValue: ", newValue, "## oldValue: ", oldValue))
+
+  // getter로
+  const a = ref(0);
+  const b = ref(0);
+  watch(() => a.value + b.value, (newval) => console.log("## sum: ", newval));
+
+  // array
+  watch([name, () => a.value + b.value], ([newVal1, newVal2]) => {
+    console.log("### newName:", newVal1);
+    console.log("### a+b:", newVal2);
+  });
+
+  const obj = reactive({
+    a: 1,
+    b: 2,
+  });
+
+  // 그냥 obj.a를 하면 얘는 단순한 숫자일 뿐
+  watch(obj.a, newVal => console.log("## newVal: ", newVal));
+  // getter로 해야 reactive로 동작
+  watch(() => obj.a, newVal => console.log("## Getter newVal: ", newVal));
+
 
 </script>
 
